69968313c37daec20feb71c3274a0a690057ac70
shubhamghimire99 <shubham0011ghimire@gmail.com>
2024-01-04 22:51:07 +0545

    navbar completed

--- src/php/Landingpage.php
+++ src/php/Landingpage.php
@@ -3,14 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" href="src/css/landingpage.css">
    
 </head>
 <body>
     <?php include 'Navbar.php';?>
+    
+    <div class="content">
 
+        <div class="categories">
+
+        </div>
+
+        <div class="slidshow">
+
+        </div>
+
+        <div class="products">
+
+      
+    </div>
+
+    </div>
+
+    <div class="justforyou">
+    
+    </div>
     
 
-    <?php include('grid.php'); ?>
+    <!-- <?php include('grid.php'); ?> -->
     
     <?php include('Footer.php'); ?>
 
