a04b20fade8ea8710f7323327c7c3a34a9bca066
migeen <migeen999@gmail.com>
2024-02-24 21:41:42 +0545

    dashboard design

--- src/seller/delete_product.php
+++ src/seller/delete_product.php
@@ -3,6 +3,13 @@
 
     if(isset($_GET['id'])){
         $id = $_GET['id'];
+        $image_name = "select image from product where product_id = $id";
+        $query_img = $conn -> query($image_name);
+        $row_img = mysqli_fetch_assoc($query_img);
+
+        $image_path = "src/images/".$row_img['image'];
+        unlink($image_path);
+
         $sql = "DELETE FROM product WHERE product_id = $id";
         if($conn->query($sql) === TRUE){
             header("Location: /inventory");
