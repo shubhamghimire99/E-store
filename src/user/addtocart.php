7f4aec48ee85c2398ee9165454e1da653a8bef29
shubhamghimire99 <shubham0011ghimire@gmail.com>
2024-03-14 20:42:30 +0545

    the bug in cart completed

--- src/user/addtocart.php
+++ src/user/addtocart.php
@@ -4,11 +4,12 @@ include "src/Database/connect.php";
 
 if (isset($_GET['id'])) {
     $id = mysqli_real_escape_string($conn, $_GET['id']);
+   echo "<script> console_log($id) </script> ";
     // view the product
     $selectproducts = "select * from product where product_id='$id'";
     $result = mysqli_query($conn, $selectproducts);
     $product = mysqli_fetch_assoc($result);
-    // echo json_encode($product);
+
 
     $user_id = $_SESSION['user_id'];
     $product_id = $product['product_id'];
@@ -19,53 +20,53 @@ if (isset($_GET['id'])) {
     $product_quantity = 1;
     $product_total = $product_price * $product_quantity;
 
-    $productexists = "SELECT * FROM cart WHERE product_id='$id' AND user_id='$user_id'";
+    $productexists = "SELECT cart_status,product_quantity FROM cart WHERE product_id='$id' AND user_id='$user_id'";
     $result_in_cart = mysqli_query($conn, $productexists);
     $product_in_cart = mysqli_fetch_all($result_in_cart , MYSQLI_ASSOC);
+    echo json_encode($product_in_cart);
 
-    if ($product_in_cart['cart_status'] == "deleted") {
-        $status_query = "UPDATE cart SET cart_status='incart'  WHERE product_id='$id' AND user_id='$user_id'";
-        $result = mysqli_query($conn, $status_query);
-        if ($result) {
-            $insert_query = "INSERT INTO notification(notification_id, buyer_id , seller_id , admin_id , product_id , order_id , cart_id , message , notification_date , notification_status) VALUES (NULL, '$user_id', '$seller_id', NULL, '$product_id', NULL, NULL, 'added to cart', CURRENT_TIMESTAMP, 'unread')";
-            $result = mysqli_query($conn, $insert_query);
-            echo "<script> alert('added to card sucessfully'); </script>";
-            header('location: /cart');
-        } else {
-            echo "Error: " . mysqli_error($conn);
+    // for each product in cart, check if the product is already in cart
+    foreach($product_in_cart as $product_in_cart){
+        if ($product_in_cart['cart_status'] == "incart") {
+            echo "<script> console.log('product already in cart'); </script>";
+            $product_quantity = $product_in_cart['product_quantity'] + 1;
+            $product_total = $product_price * $product_quantity;
+            $sql = "UPDATE cart SET product_quantity='$product_quantity', product_total='$product_total' WHERE product_id='$id' AND user_id='$user_id'";
+            if ($conn->query($sql) === TRUE) {
+                echo "<script> console.log('product added to cart updated quantity'); </script>";
+                header('location: /cart');
+            } else {
+                echo "Error: " . mysqli_error($conn);
+            }
         }
+
+        if ($product_in_cart != null && $product_in_cart['cart_status'] == "incart") {
+            echo "<script> console.log('product already in cart'); </script>";
+            $product_quantity = $product_in_cart['product_quantity'] + 1;
+            $product_total = $product_price * $product_quantity;
+            $sql = "UPDATE cart SET product_quantity='$product_quantity', product_total='$product_total' WHERE product_id='$id' AND user_id='$user_id'";
+            if ($conn->query($sql) === TRUE) {
+                echo "<script> console.log('product added to cart updated quantity'); </script>";
+                header('location: /cart');
+            } else {
+                echo "Error: " . mysqli_error($conn);
+            }
+        } 
+            
     }
 
-    if ($product_in_cart != null && $product_in_cart['cart_status'] == "incart") {
-        $product_quantity = $product_in_cart['product_quantity'] + 1;
-        $product_total = $product_price * $product_quantity;
-        $sql = "UPDATE cart SET product_quantity='$product_quantity', product_total='$product_total' WHERE product_id='$id' AND user_id='$user_id'";
-        if ($conn->query($sql) === TRUE) {
-            echo "<script> alert('added to card sucessfully'); </script>";
-            $insert_query = "INSERT INTO notification(notification_id, buyer_id , seller_id , admin_id , product_id , order_id , cart_id , message , notification_date , notification_status) VALUES (NULL, '$user_id', '$seller_id', NULL, '$product_id', NULL, NULL, 'added to cart', CURRENT_TIMESTAMP, 'unread')";
-            $result = mysqli_query($conn, $insert_query);
-            // post the message in the notification data base
-            header('location: /cart');
-        } else {
-            echo "Error: " . mysqli_error($conn);
+    // if product is not in cart, add product to cart
+    if ($product_in_cart == null) {
+            $sql = "INSERT INTO cart (cart_id,user_id, product_id, product_name, 
+            product_price, product_image, product_quantity, product_total,cart_status)
+                     VALUES (NULL,'$user_id', '$product_id', '$product_name', 
+                     '$product_price', '$product_image', '$product_quantity', '$product_total','incart')";
+            if ($conn->query($sql) === TRUE) {
+                echo "<script> console.log('product added to cart'); </script>";
+                header('location: /cart');
+            } else {
+                echo "Error:" . mysqli_error($conn);
+            }
         }
-    } 
-        
-    else {
-        $sql = "INSERT INTO cart (cart_id,user_id, product_id, product_name, 
-        product_price, product_image, product_quantity, product_total,cart_status)
-                 VALUES (NULL,'$user_id', '$product_id', '$product_name', 
-                 '$product_price', '$product_image', '$product_quantity', '$product_total','incart')";
-        if ($conn->query($sql) === TRUE) {
-            // header('location: /cart');
-            // post name of product and image in the notification data base
-            $insert_query = "INSERT INTO notification(notification_id, buyer_id , seller_id , admin_id , product_id , order_id , cart_id , message , notification_date , notification_status) VALUES (NULL, '$user_id', NULL , NULL, '$product_id', NULL, NULL, 'added to cart', CURRENT_TIMESTAMP, 'unread')";
-            $result = mysqli_query($conn, $insert_query);
-            echo "<script> alert('added to card sucessfully'); </script>";
-
-            header('location: /cart');
-        } else {
-            echo "Error:" . mysqli_error($conn);
-        }
-    }
+   
 }
